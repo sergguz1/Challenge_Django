2841b27aea70f94a66416b150e399e503bfbf3f8
sergguz@gmail.com <sergioroberto1>
2023-05-03 02:19:31 -0400

    adding static

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path("january", views.january),
@@ -7,5 +9,5 @@ urlpatterns = [
     path("",views.index, name="index"), #/challenges/
     path("<int:month>", views.monthly_challenges_by_number),
     path("<str:month>", views.monthly_challenge, name="month-string")
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
