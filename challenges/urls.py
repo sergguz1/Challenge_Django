5b92d78f52e85a6765760d27025e3dc00531fc03
sergguz@gmail.com <sergioroberto1>
2023-05-04 10:06:23 -0400

    fixing project

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     # path("january", views.january),
@@ -9,5 +9,7 @@ urlpatterns = [
     path("",views.index, name="index"), #/challenges/
     path("<int:month>", views.monthly_challenges_by_number),
     path("<str:month>", views.monthly_challenge, name="month-string")
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
